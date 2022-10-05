00d7b1695449f09694c6346d154e39fe12a8e16d
Min RK <benjaminrk@gmail.com>
2022-10-04 23:31:17 -0700

    add lab extension

--- jupyter_keepalive/__init__.py
+++ jupyter_keepalive/__init__.py
@@ -1,8 +1,20 @@
 __version__ = "0.0.1.dev"
 
+
 from jupyter_server.utils import url_path_join
 
 from .handlers import KeepAliveHandler
+
+
+def _jupyter_labextension_paths():
+    import json
+    from pathlib import Path
+
+    _HERE = Path(__file__).parent.resolve()
+
+    with (_HERE / "labextension" / "package.json").open() as f:
+        pkg = json.load(f)
+    return [{"src": "labextension", "dest": pkg["name"]}]
 
 
 def _jupyter_server_extension_points():
@@ -12,5 +24,5 @@ def _jupyter_server_extension_points():
 def _load_jupyter_server_extension(serverapp):
     web_app = serverapp.web_app
     host_pattern = ".*$"
-    route_pattern = url_path_join(web_app.settings["base_url"], r"/api/ext-keepalive")
+    route_pattern = url_path_join(web_app.settings["base_url"], r"/ext-keepalive")
     web_app.add_handlers(host_pattern, [(route_pattern, KeepAliveHandler)])
---
+++ src/index.ts
@@ -0,0 +1,124 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin,
+} from "@jupyterlab/application";
+
+import { ICommandPalette } from "@jupyterlab/apputils";
+
+import { URLExt } from "@jupyterlab/coreutils";
+
+import { ServerConnection } from "@jupyterlab/services";
+
+/**
+ * Call the API extension
+ *
+ * @param endPoint API REST end point for the extension
+ * @param init Initial values for the request
+ * @returns The response body interpreted as JSON
+ */
+async function keepAliveRequest(endPoint = "", init = {}) {
+  // Make request to Jupyter API
+  const settings = ServerConnection.makeSettings();
+  const requestUrl = URLExt.join(settings.baseUrl, "ext-keepalive", endPoint);
+
+  let response: Response;
+  try {
+    response = await ServerConnection.makeRequest(requestUrl, init, settings);
+  } catch (error) {
+    throw new ServerConnection.NetworkError(error);
+  }
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new ServerConnection.ResponseError(response, data.message);
+  }
+
+  return data;
+}
+
+async function keepAliveStart() {
+  return await keepAliveRequest("", { method: "POST" });
+}
+
+async function keepAliveStop() {
+  return await keepAliveRequest("", { method: "DELETE" });
+}
+
+async function keepAliveRemaining() {
+  return await keepAliveRequest("");
+}
+
+/**
+ * Initialization data for the server-extension-example extension.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: "jupyter-keepalive",
+  autoStart: true,
+  optional: [],
+  requires: [ICommandPalette],
+  activate: async (app: JupyterFrontEnd, palette: ICommandPalette) => {
+    console.log("JupyterLab extension keepalive is activated!");
+
+    // GET request
+    try {
+      const data = await keepAliveRemaining();
+      console.log(data);
+    } catch (reason) {
+      console.error(`Error on GET /api/.\n${reason}`);
+    }
+
+    // POST request
+    const dataToSend = { name: "George" };
+    try {
+      const reply = await keepAliveStart();
+      console.log(reply);
+    } catch (reason) {
+      console.error(
+        `Error on POST /jlab-ext-example/hello ${dataToSend}.\n${reason}`
+      );
+    }
+
+    const { commands } = app;
+    const category = "Keepalive";
+
+    commands.addCommand("keepalive:start", {
+      label: "Keep server alive while idle",
+      caption:
+        "Registers activity so idle cullers don't shut this server down.",
+      execute: () => {
+        // todo: prompt for interval
+        // send as JSON.stringify({seconds: n})
+        // maybe that should be minutes? who keeps alive for seconds...?
+        keepAliveStart();
+      },
+    });
+
+    commands.addCommand("keepalive:stop", {
+      label: "Stop keeping server alive",
+      caption: "Stop the keepalive spinner",
+      execute: () => {
+        keepAliveStop();
+      },
+    });
+
+    commands.addCommand("keepalive:check", {
+      label: "Check keepalive status",
+      caption: "Check the remaining time on the ",
+      execute: () => {
+        keepAliveRemaining();
+        // todo: display it somehow
+      },
+    });
+
+    for (var command of [
+      "keepalive:start",
+      "keepalive:stop",
+      "keepalive:check",
+    ]) {
+      palette.addItem({ command: command, category: category });
+    }
+  },
+};
+
+export default extension;
